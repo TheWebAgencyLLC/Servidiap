d44633a7197b36849cdde7f79e2a0c141c4cb649
Sneh <sneh.mehta23@gmail.com>
2025-03-29 13:53:47 -0500

    feat: removed default dashboard template stuff mainly - start from here

---
+++ my-dashboard/app/pages/index.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { sub } from 'date-fns'
+import type { Period, Range } from '~/types'
+
+const items = [[{
+  label: 'New Quote',
+  icon: 'i-lucide-notepad-text',
+  to: '/quote'
+}, {
+  label: 'Add Supplies',
+  icon: 'i-lucide-briefcase-medical',
+  to: '/supplies'
+}]]
+
+const range = shallowRef<Range>({
+  start: sub(new Date(), { days: 14 }),
+  end: new Date()
+})
+const period = ref<Period>('daily')
+</script>
+
+<template>
+  <UDashboardPanel id="home">
+    <template #header>
+      <UDashboardNavbar title="Dashboard" :ui="{ right: 'gap-3' }">
+        <template #leading>
+          <UDashboardSidebarCollapse />
+        </template>
+
+        <template #right>
+          <UDropdownMenu :items="items">
+            <UButton icon="i-lucide-plus" size="md" class="rounded-full" />
+          </UDropdownMenu>
+        </template>
+      </UDashboardNavbar>
+    </template>
+
+    <template #body>
+      <!-- <HomeStats :period="period" :range="range" />
+      <HomeChart :period="period" :range="range" />
+      <HomeSales :period="period" :range="range" /> -->
+    </template>
+  </UDashboardPanel>
+</template>
---
+++ my-dashboard/app/pages/quote.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+  const items = [[{
+  label: 'New Quote',
+  icon: 'i-lucide-notepad-text',
+  to: '/quote'
+}, {
+  label: 'Add Supplies',
+  icon: 'i-lucide-briefcase-medical',
+  to: '/supplies'
+}]]
+</script>
+
+<template>
+  <UDashboardPanel id="quote">
+    <template #header>
+      <UDashboardNavbar title="Create Quote" :ui="{ right: 'gap-3' }">
+        <template #leading>
+          <UDashboardSidebarCollapse />
+        </template>
+
+        <template #right>
+          <UDropdownMenu :items="items">
+            <UButton icon="i-lucide-plus" size="md" class="rounded-full" />
+          </UDropdownMenu>
+        </template>
+      </UDashboardNavbar>
+    </template>
+
+    <template #body>
+      <!-- <HomeStats :period="period" :range="range" />
+      <HomeChart :period="period" :range="range" />
+      <HomeSales :period="period" :range="range" /> -->
+    </template>
+  </UDashboardPanel>
+</template>
